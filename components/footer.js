e23169b8f7856730af29f931abfc32c8fa122783
JFLeon <jofule@uoc.edu>
2024-05-27 13:04:43 +0200

    navigation and work experience added

---
+++ components/footer.js
@@ -0,0 +1,33 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+  <style>
+    footer {
+      height: 60px;
+      padding: 0 10px;
+      list-style: none;
+      display: flex;
+      flex-shrink: 0;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #f1f1f1;
+    }
+  </style>
+  <footer>
+    <p>Last update: June 2024.</p>
+  </footer>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    shadowRoot.appendChild(footerTemplate.content);
+  }
+}
+
+customElements.define('footer-component', Footer);

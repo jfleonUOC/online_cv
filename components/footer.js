d5e9310adf7d7dd4e2b016dffb909a7a0e07d32e
JFLeon <jofule@uoc.edu>
2024-06-11 18:26:50 +0200

    link format modified

--- components/footer.js
+++ components/footer.js
@@ -8,10 +8,22 @@ footerTemplate.innerHTML = `
       padding: 10px;
       list-style: none;
       display: flex;
+      // float: none;
       flex-shrink: 0;
       justify-content: space-between;
       align-items: center;
       background-color: #f1f1f1;
+    }
+    a {
+      font-weight: 700;
+      margin: 0 25px;
+      color: #000000;
+      text-decoration: none;
+      box-shadow: inset 0 -1px 0 0 #ff0000;
+    }
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -4px 0 0 #ff0000;
     }
     @media screen and (max-width: 600px) {
     footer {
@@ -22,8 +34,8 @@ footerTemplate.innerHTML = `
   </style>
   <footer>
     <p>Mail: Jo Fu Le 8 at gmail</p>
-    <a href="https://www.linkedin.com/in/jonasfuentesleon" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-    <a href="https://github.com/jfleonUOC/" target="_blank" rel="noopener noreferrer">GitHub</a>
+    <p><a href="https://www.linkedin.com/in/jonasfuentesleon" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+    <p><a href="https://github.com/jfleonUOC/" target="_blank" rel="noopener noreferrer">GitHub</a></p>
     <p>Last update: June 2024.</p>
   </footer>
 `;
--- components/header.js
+++ components/header.js
@@ -7,7 +7,8 @@ headerTemplate.innerHTML = `
       display: flex;
       align-items: center;
       justify-content: center;
-      background-color:  #0a0a23;
+      // background-color:  #0a0a23;
+      background-color:#000000
     }
     li {
       list-style: none;
@@ -19,10 +20,11 @@ headerTemplate.innerHTML = `
       margin: 0 25px;
       color: #fff;
       text-decoration: none;
+      box-shadow: inset 0 -1px 0 0 #ff0000;
     }
     a:hover {
-      padding-bottom: 5px;
-      box-shadow: inset 0 -2px 0 0 #fff;
+      padding-bottom: 1px;
+      box-shadow: inset 0 -4px 0 0 #ff0000;
     }
     @media screen and (max-width: 600px) {
     li {
@@ -37,7 +39,7 @@ headerTemplate.innerHTML = `
   <header>
     <nav>
       <ul>
-        <li><a href="index.html">About</a></li>
+        <li><a href="index.html">About Me</a></li>
         <li><a href="work_experience.html">Work</a></li>
 	    	<li><a href="education.html">Education</a></li>
         <li><a href="publications.html">Publications</a></li>

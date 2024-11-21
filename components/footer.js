0d8fb5297ab4ce4981370e5881121cf64da270bb
JFLeon <jofule@uoc.edu>
2024-11-21 16:48:26 +0100

    correct typos

--- components/footer.js
+++ components/footer.js
@@ -51,7 +51,7 @@ footerTemplate.innerHTML = `
     <p>Last update: November 2024.</p>
   </footer>
     <div class="license">
-    <p xmlns:cc="http://creativecommons.org/ns#" >This work by <span property="cc:attributionName">Jonas Fuentes Leon</span> is licensed under: <a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""></a></p>
+    <p xmlns:cc="http://creativecommons.org/ns#" >This web by <span property="cc:attributionName">Jonas Fuentes Leon</span> is licensed under: <a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""></a></p>
     </div>
 `;
 
